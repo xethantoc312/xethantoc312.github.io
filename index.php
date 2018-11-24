245ff171d3b6db85374ef5d7bf5b88ff6c1834ec
Bao Lam <lamquocbao312@gmail.com>
2018-11-24 21:43:09 +0700

    upload
    hoan thanh

--- index.php
+++ index.php
@@ -1,124 +1,146 @@
 <?php
 /**
- * @author Nguyen Duc Hanh
- * @copyright 2014
- * @copygith nguyenduchanh.com
+ * VFM - veno file manager index
+ *
+ * PHP version >= 5.3
+ *
+ * @category  PHP
+ * @package   VenoFileManager
+ * @author    Nicola Franchini <info@veno.it>
+ * @copyright 2013 Nicola Franchini
+ * @license   Exclusively sold on CodeCanyon: https://codecanyon.net/item/veno-file-manager-host-and-share-files/6114247
+ * @link      http://filemanager.veno.it/
  */
-// delete file guest download on yesterday
-//include_once('cron_delete_file.php');
-
-session_start();
-error_reporting(0);
-include_once('functions.php');
-
-if (isset($_GET['url'])) {
-    $url = $_GET['url'];
-    if (strpos($url, 'http') === false) {
-        $url = 'http://' . $url;
-    }
-    if (filter_var($url, FILTER_VALIDATE_URL) === FALSE) {
-        $err_mgs[] = '+ Nhập đường dẫn url chưa hợp lệ';
-    }else{
-        $_SESSION['url'] = $url;
-    }
-    
-
-    $parse_url = parse_url($url);
-    $folder = $parse_url['host'];
-    if ($folder == '') {
-        $err_mgs[] = '+ Tên thư mục không hợp lệ';
-    } else {
-        $_SESSION['folder'] = $folder;
-    }
-
-    $save_file = $_SESSION['save_file']  = 'index.html';
-    if ($url && $folder && $save_file) {
-        header("location: result.php");
-    }
-} else {
-    session_destroy();
-}
-
-/* delete folder after 5 minutes */
-$dirs = array_filter(glob('save/*'), 'is_dir');
-foreach($dirs as  $fd){
-    $time_created = filemtime($fd);
-    if(time() - 600 > $time_created){
-       system("rm -rf ".escapeshellarg($fd));
-    }
-    
-}
+define('VFM_APP', true);
+require_once 'vfm-admin/include/head.php';
 ?>
-<!DOCTYPE html>
-<html lang="vi">
-    <head>
-        <meta content="text/html; charset=utf-8" http-equiv="Content-Type" />
-        <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=1" name="viewport" />
-        <link href="http://nguyenduchanh.com/css-spider/" rel="canonical" />
-        <meta content="index, follow" name="ROBOTS" />
-        <meta content="900" http-equiv="refresh" />
-        <meta http-equiv="Cache-Control" content="max-age=2592000, public" />
-        <title>CSS Spider | Nguyễn Đức Hạnh</title>
-        <meta name="keywords" content="Css Spider, clone theme, clone css" >
-        <meta name="description" content="Css Spider, công cụ clone nhanh giao diện website" />
-        <meta property="og:image" content="http://nguyenduchanh.com/css-spider/asset/images/logo.png" />
-        <meta name="geo.region" content="VN-HN" />
-        <meta name="geo.placename" content="Hà Nội" />
-        <meta name="geo.position" content="21.027764;105.83416" />
-        <meta name="ICBM" content="21.027764, 105.83416" />
-        <link href="asset/images/favicon.png" rel="icon" type="image/png" />
-        <link rel="stylesheet" href="asset/css/main.css" media=all />
-    </head>
-    <body>
-        <div id="content" style="margin-top: 10%;">
-            <form name="frm" id="frm" action="" method="get">
-                <div class="row">
-                    <a href="/css-spider"><img src="asset/images/logo.png" height="120" alt="Css Spider" /></a>
-                </div>
-                <div class="row">
-                    <p class="red">
-                    <?php
-                    if (isset($err_mgs)) {
-                        foreach ($err_mgs as $msg) {
-                            echo $msg . '<br/>';
-                        }
-                    }
-                    ?>
-                    </p>
-                </div>
-                <div class="row wrap-input">
-                    <input type="text" name="url" placeholder="Nhập url website" />
-                </div>
-                <div class="row wrap-submit">
-                    <input type="reset" value="Xóa" />
-                    <input type="submit" value="Bắt đầu Xử lý" />
-                </div>
-            </form>
-        </div>
-        <div id="sidebar">
-            <ul>
-                <?php
-                $allZipFile = getZipFile();
-                foreach ($allZipFile as $key=>$value){
-                    echo '<li><a href="'.$value.'">'.substr($value, 5).'</a></li>';
+<!doctype html>
+<html lang="<?php echo $setUp->lang; ?>">
+<head>
+    <title><?php echo $setUp->getConfig("appname"); ?></title>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <?php echo $setUp->printIcon("vfm-admin/_content/uploads/"); ?>
+    <meta name="description" content="file manager">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="vfm-admin/css/bootstrap.min.css">
+    <link rel="stylesheet" href="vfm-admin/vfm-style.css">
+    <?php 
+    if ($setUp->getConfig("txt_direction") == "RTL") { ?>
+        <link rel="stylesheet" href="vfm-admin/css/bootstrap-rtl.min.css">
+    <?php 
+    } ?>
+    <link rel="stylesheet" href="vfm-admin/css/font-awesome.min.css">
+    <link rel="stylesheet" href="vfm-admin/_content/skins/<?php echo $setUp->getConfig('skin') ?>">
+    <script type="text/javascript" src="vfm-admin/js/jquery-3.3.1.min.js"></script>
+    <!--[if lt IE 9]>
+    <script src="vfm-admin/js/html5.js" type="text/javascript"></script>
+    <script src="vfm-admin/js/respond.min.js" type="text/javascript"></script>
+    <![endif]-->
+    <?php
+    $bodyclass = 'vfm-body';
+    if ($setUp->getConfig('inline_thumbs') == true) {
+        $bodyclass .= ' inlinethumbs';
+    }
+    if (!$gateKeeper->isAccessAllowed()) {
+        $bodyclass .= ' unlogged';
+    }
+    $audioping = $setUp->getConfig('audio_notification');
+    if ($audioping) { ?>
+    <script type="text/javascript">
+        var audio_ping = new Audio("vfm-admin/<?php echo $setUp->getConfig('audio_notification'); ?>");
+    </script>
+    <?php
+    } ?>
+</head>
+    <body id="uparea" class="<?php echo $bodyclass; ?>">
+        <div class="overdrag"></div>
+            <?php
+            /**
+            * ************************************************
+            * ******************** HEADER ********************
+            * ************************************************
+            */ 
+            $template->getPart('activate');
+            $template->getPart('navbar');
+            $template->getPart('header');
+            ?>
+        <div class="container">
+            <?php
+            /**
+            * ************************************************
+            * **************** Response messages *************
+            * ************************************************
+            */
+            ?>
+            <div id="error">
+                <noscript>
+                    <div class="response boh">
+                        <span><i class="fa fa-exclamation-triangle"></i> Please activate JavaScript</span>
+                    </div>
+                </noscript>
+                <?php echo $setUp->printAlert(); ?>
+            </div>
+            <div class="main-content">
+            <?php 
+            $getrp = filter_input(INPUT_GET, "rp", FILTER_SANITIZE_STRING);
+            $getreg = filter_input(INPUT_GET, "reg", FILTER_SANITIZE_STRING);
+
+            if ($getdownloadlist) :
+                /**
+                * ************************************************
+                * ********* SHOW FILE SHARING DOWNLOADER *********
+                * ************************************************
+                */
+                $template->getPart('downloader');
+
+            elseif ($getrp) :
+                /**
+                * ************************************************
+                * **************** PASSWORD RESET ****************
+                * ************************************************
+                */
+                $template->getPart('reset');
+            else :
+                /**
+                * ************************************************
+                * **************** SHOW FILEMANAGER **************
+                * ************************************************
+                */
+                if (!$getreg || $setUp->getConfig('registration_enable') == false) {
+                    $template->getPart('user-redirect');
+                    $template->getPart('remote-uploader');
+                    $template->getPart('notify-users');
+                    $template->getPart('uploadarea');
+                    $template->getPart('breadcrumbs');
+                    $template->getPart('list-folders');
+                    $template->getPart('list-files');
+                    $template->getPart('disk-space');
                 }
-                ?>
-            </ul>
-        </div>
-        
-        <script type="text/javascript" src="asset/js/jquery.min.js"></script>
-        <script type="text/javascript" src="asset/js/enscroll-0.6.1.min.js"></script>
-        <script type="text/javascript" src="asset/js/main.js"></script>
-        
-        <script>
-        (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-        (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-        m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-        })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+                if ($getreg && $setUp->getConfig('registration_enable') == true) {
+                    $template->getPart('register');
+                } else {
+                    $template->getPart('login');
+                }
+            endif; ?>
+            </div> <!-- .main-content -->
+        </div> <!-- .container -->
+        <?php
+        /**
+        * ************************************************
+        * ******************** FOOTER ********************
+        * ************************************************
+        */
+        $template->getPart('footer');
+        $template->getPart('load-js');
+        $template->getPart('modals');
 
-        ga('create', 'UA-68158191-1', 'auto');
-        ga('send', 'pageview');
-
-        </script>
+        // Audio notification after upload
+        if ($audioping && isset($_GET['response'])) { ?>
+            <script type="text/javascript">
+                audio_ping.play();
+            </script>
+        <?php
+        } ?>
     </body>
 </html>
